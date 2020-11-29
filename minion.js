8a32de29d9b59974aa1f80da3a82bb09d240f7e3
Akhil-Movva <akhilmovva@gmail.com>
2020-11-29 19:12:02 +0530

    added a new repository with a language translation application.

---
+++ minion.js
@@ -0,0 +1,35 @@
+var btnClick=document.querySelector("#btn");
+
+var text = document.querySelector("#text-input");
+
+var output = document.querySelector("#text-output");
+
+ 
+function getURL(text){
+
+    return "https://api.funtranslations.com/translate/minion.json" + "?" +"text=" + text;
+}
+function errorHandler(error){
+   console.log("Error has been encountered", error);
+   alert("Something gone wrong. Please try again after some time");
+}
+
+function clickHandler(){
+    
+      fetch(getURL(text.value))
+       .then(response => response.json())
+       .then(json =>{
+           output.innerText= json.contents.translated;
+       })
+       .catch(errorHandler); 
+    
+}
+
+
+
+btnClick.addEventListener("click", clickHandler);
+
+
+
+
+
